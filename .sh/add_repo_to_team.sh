70669f3ce76d8e6221d1864114da8976c874e597
Maxime RAUCH <maxime.rauch@chargemap.com>
2024-01-24 13:13:40 +0100

    feat(script): add script to add repositories to team

--- .sh/add_repo_to_team.sh
+++ .sh/add_repo_to_team.sh
@@ -14,7 +14,7 @@ echo "Starting the script..."
 for repo in "${repos[@]}"; do
 
   # Use a try-catch mechanism
-  response=$(curl -s -w "%{http_code}" \
+  status_code=$(curl -s -w "%{http_code}" \
       -X PUT \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $TOKEN" \
@@ -22,7 +22,7 @@ for repo in "${repos[@]}"; do
       -d '{"permission":"admin"}' \
       "$BASE_URL/$OWNER/$repo")
 
-  if [ "$status_code" -eq 200 ]; then
+  if [ "$status_code" -eq 204 ]; then
       echo "Repository '$repo' successfully added to team: '$TEAM_SLUG'"
   else
       echo "Failed to add repository '$repo' to team: '$TEAM_SLUG' (HTTP Status Code: $status_code)"
